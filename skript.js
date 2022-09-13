f3b1efba5973816a7647abd3ea3af5c3af309d7a
buenni86 <12584248+buenni86@users.noreply.github.com>
2022-09-13 10:45:57 +0200

    add skript.js

---
+++ skript.js
@@ -0,0 +1,13 @@
+import { } from "https://unpkg.com/@workadventure/scripting-api-extra@^1";
+
+var currentPopup = undefined;
+var isCoWebSiteOpened =  false;
+
+function closePopUp(){
+    if (currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+    }
+}
+
+
